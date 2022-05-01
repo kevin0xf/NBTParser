19b22ef101f33816b78afa2469673414e9f72996
kevin0xf <kevin.salmen136@gmail.com>
2022-05-01 19:15:55 +0200

    Update

--- src/Main.java
+++ src/Main.java
@@ -10,16 +10,15 @@ public class Main {
 
     public static void main(String[] args) {
         File file = new File("resources/servers.dat");
-        FileInputStream fileInputStream = null;
         byte[] bytes = null;
         try {
-            fileInputStream = new FileInputStream(file);
+            FileInputStream fileInputStream = new FileInputStream(file);
             bytes = fileInputStream.readAllBytes();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         NBTReader nbtReader = new NBTReader(bytes);
         NBTCompound nbtCompound = nbtReader.parse();
-
     }
 }
